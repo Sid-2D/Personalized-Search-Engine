6fe571f38d98132d2f6fc2e474de38676398167c
Sid <sid6327@gmail.com>
2017-10-21 23:54:22 +0530

    Fixing result strings and adding weights.

--- Backend/tfidf.py
+++ Backend/tfidf.py
@@ -1,16 +1,20 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 import json
 import pprint
+import re
 
 def filterResults(results):
 	arr = []
 	for item in results['results']['items']:
-		# arr.append({
-		# 	'title': item['title'],
-		# 	'snippet': item['snippet']
-		# })
 		# filter out dates from snippet here
-		arr.append(item['snippet'])
+		snippet = item['snippet']
+		match = re.search(r'\.\.\..+', snippet)
+		if match:
+			print 'Date found:\n', match.group()
+			arr.append(match.group()[4:])
+		else:
+			arr.append(snippet)
 	return arr
 
 def findSimiliarity(results):
@@ -25,7 +29,19 @@ def findSimiliarity(results):
 	for item in filteredResults:
 		print '\n', i, item
 		i += 1
-	print '\n', pairwise_similarity[:, 0]
+	weights = pairwise_similarity[:, 0]
+	print '\n', weights
+	# Add weights to results
+	weightedList = []
+	for i in range(len(filteredResults)):
+		weightedList.append({
+			'result': filteredResults[i],
+			'weight': weights[i]
+		})
+	# Sort the array w.r.t weights
+	# print sorted(weightedList, key=weights)
+	weightedList.sort()
+	print weightedList
 
 if __name__ == '__main__':
 	fp = open('../personalizeData.json')
